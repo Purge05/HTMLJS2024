b7d2a6f6c5c7b8ec6b7b131ff587e9d2f08d443a
Your Name <you@example.com>
2024-03-07 08:44:27 -0500

    End of Week 9

---
+++ Week9/slideshow.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    document.querySelector('h1').innerHTML = "South Park Slideshow";
+
+    var image = 0;
+    var images = [
+        "KennyMcCormick.png",
+        "eric-cartman_380.jpg",
+        "KyleBroflovski.png",
+        "StanMarsh.png"
+    ];
+    var siteBtn = document.querySelector('#slideshow-button');
+
+    function changeImage(){
+        siteBtn.removeEventListener('click', changeImage);
+        if(image >= images.length){
+            image = 0;
+        }
+        
+        document.querySelector("#site-image").src = `images/${images[image]}`;
+        setTimeout(changeImage, 2000);
+        image++;
+    }
+    siteBtn.addEventListener('click', changeImage)
+}
